715a5776e4a464e73c686d2ce41fa595f2f7c736
Vidish_Bajpai <kushbajpai641@gmail.com>
2025-05-11 20:45:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ import streamlit.components.v1 as components
 model = joblib.load('Cognitive_Model.pkl')  
 df = pd.read_csv('Cleaned_Cognitive.csv')  
 
-st.title("Cognify 🧠 - A Project by Vidish Bajpai.")
+st.title("Cognify 🧠 ")
+st.caption("A Project by Vidish Bajpai")
 st.write("""
     Cognify is an intelligent tool designed to analyze and enhance human cognitive performance. Powered by data science and AI, 
     this interactive platform evaluates key factors like sleep patterns, stress levels, screen time, memory scores, your diet, 
